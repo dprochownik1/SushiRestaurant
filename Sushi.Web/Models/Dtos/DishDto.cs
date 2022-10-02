69b9300d5c28ebefbcbc09547c9beb7e1f0f2593
Dominik Prochownik <d.prochownik17@gmail.com>
2022-10-02 20:50:13 +0200

    Base models in web app

---
+++ Sushi.Web/Models/Dtos/DishDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sushi.Web.Models.Dtos
+{
+    public class DishDto
+    {
+        [Key]
+        public int DishId { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public string CategoryName { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ Sushi.Web/StaticDetails.cs
@@ -0,0 +1,15 @@
+﻿namespace Sushi.Web
+{
+    public class StaticDetails
+    {
+        public static string DishApiBase { get; set; }
+
+        public enum ApiType
+        {
+            GET,
+            POST,
+            PUT,
+            DELETE,
+        }
+    }
+}
