b51cf940c69768e430f2b60d0f091b9e0bd77c25
Dominik Prochownik <d.prochownik17@gmail.com>
2022-10-02 15:14:52 +0200

    Add project files.

---
+++ Sushi.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sushi.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
