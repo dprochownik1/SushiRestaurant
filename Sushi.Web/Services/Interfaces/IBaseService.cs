6e29128d1fffaf006b11d8eebf1468a2673cec44
Dominik Prochownik <d.prochownik17@gmail.com>
2022-10-02 20:50:49 +0200

    Add IBaseService and IDishService interfaces

---
+++ Sushi.Web/Services/Interfaces/IBaseService.cs
@@ -0,0 +1,11 @@
+﻿using Sushi.Web.Models;
+using Sushi.Web.Models.Dtos;
+
+namespace Sushi.Web.Services.Interfaces
+{
+    public interface IBaseService : IDisposable
+    {
+        ResponseDto responseDto { get; set; }
+        Task<T> SendAsync<T>(ApiRequest apiRequest);
+    }
+}
---
+++ Sushi.Web/Services/Interfaces/IDishService.cs
@@ -0,0 +1,14 @@
+﻿using Sushi.Web.Models.Dtos;
+
+namespace Sushi.Web.Services.Interfaces
+{
+    public interface IDishService : IBaseService
+    {
+        Task<T> GetAllDishesAsync<T>();
+        Task<T> GetDishByIdAsync<T>(int dishId);
+        Task<T> CreateDishAsync<T>(DishDto dishDto);
+        Task<T> UpdateDishAsync<T>(DishDto dishDto);
+        Task<T> DeleteDishAsync<T>(int dishId);
+    }
+}
+ 
