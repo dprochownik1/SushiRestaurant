4b468a92f1c5e0e63a77bf844f49b76276d0b793
Dominik Prochownik <d.prochownik17@gmail.com>
2022-10-03 10:44:23 +0200

    Implement DishController

---
+++ Sushi.Web/Controllers/DishController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Sushi.Web.Models.Dtos;
+using Sushi.Web.Services.Interfaces;
+
+namespace Sushi.Web.Controllers
+{
+    
+    public class DishController : Controller
+    {
+        private readonly IDishService _dishService;
+        public DishController(IDishService dishService)
+        {
+            _dishService = dishService;
+        }
+        public async Task<IActionResult> DishIndex()
+        {
+            var dishes = new List<DishDto>();
+            var response = await _dishService.GetAllDishesAsync<ResponseDto>();
+            if (response != null && response.IsSuccess)
+            {
+                dishes = JsonConvert.DeserializeObject<List<DishDto>>(Convert.ToString(response.Result));
+            }
+            return View(dishes);
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(DishDto dishDto)
+        {
+            var response = await _dishService.CreateDishAsync<ResponseDto>(dishDto);
+            if (response != null && response.IsSuccess)
+            {
+                return RedirectToAction(nameof(DishIndex));
+            }
+            return View(dishDto);
+        }
+    }
+}
