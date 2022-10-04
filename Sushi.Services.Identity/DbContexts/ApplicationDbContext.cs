5fdc46ef749732c7f02c256dfaf07f16189f1995
Dominik Prochownik <d.prochownik17@gmail.com>
2022-10-04 15:47:23 +0200

    initial commit for Sushi.Services.Identity project

---
+++ Sushi.Services.Identity/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Sushi.Services.Identity.Models;
+
+namespace Sushi.Services.Identity.DbContexts
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
---
+++ Sushi.Services.Identity/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Sushi.Services.Identity.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+
+    }
+}
