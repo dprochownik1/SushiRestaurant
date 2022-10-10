5e835a8962e2accd9be8d4dfce147f89de5a511f
Dominik Prochownik <d.prochownik17@gmail.com>
2022-10-10 20:27:49 +0200

    Remove reduntant property

--- Sushi.Services.ShoppingCartAPI/Models/Dtos/CartHeaderDto.cs
+++ Sushi.Services.ShoppingCartAPI/Models/Dtos/CartHeaderDto.cs
@@ -5,6 +5,5 @@
         public int CartHeaderId { get; set; }
         public string UserId { get; set; }
         public string CouponCode { get; set; }
-        public double OrderTotal { get; set; }
     }
 }
